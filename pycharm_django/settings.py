620bd1d3070558a725e7c1613e7013ba3d98582e
context-dependent <tmcmanus@BPA.LOCAL>
2018-12-12 14:58:22 -0500

    Heroky requirements part 10

--- pycharm_django/settings.py
+++ pycharm_django/settings.py
@@ -125,11 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = ( os.path.join('static'), )
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
